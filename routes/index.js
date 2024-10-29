535cd9f132861c24955d00abad86d2d2d17fdad6
Jackson Barker <jackson@jacksonbarker.ca>
2024-10-29 13:33:03 -0400

    Migrate assignment 1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index");
+});
+
+router.get("/about", function (req, res, next) {
+  res.render("about");
+});
+
+router.get("/contact", function (req, res, next) {
+  res.render("contact");
+});
+
+router.get("/projects", function (req, res, next) {
+  res.render("projects");
 });
 
 module.exports = router;
